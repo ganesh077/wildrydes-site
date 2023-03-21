50b7d9e18052b088750285c2a6b80f746fb2011b
ganesh077 <ganeshthampi077@gmail.com>
2023-03-21 20:41:55 +0000

    config changes

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_yw6he9nQ2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2tbhk4ignilr5n3m65f9tocd1g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ganesh077.auth.us-east-1.amazoncognito.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
