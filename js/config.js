db9e9613d0d810049486f8436154c9812607a218
ganesh077 <ganeshthampi077@gmail.com>
2023-03-21 20:55:05 +0000

    config changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_yw6he9nQ2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2tbhk4ignilr5n3m65f9tocd1g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_sj3T3LTVN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '8sj56bs3il46v6ino52gvv329', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
